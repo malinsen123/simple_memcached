ca52ac050682da4eee6041926e2e39abbd6098a5
malinsen123 <59331775+malinsen123@users.noreply.github.com>
2020-09-01 21:27:36 -0400

    Update simple_memcached.c

--- simple_memcached.c
+++ simple_memcached.c
@@ -194,7 +194,8 @@ void stat_print(stat* stats) {
 }
 
 static stat* stats_initial(uint64_t hash_power_value){
-    stat* stats = stat_default;
+    stat* stats;
+    *stats = stat_default;
     stats->hash_power_value= hash_power_value ;
     stats->slab_factor = 1.2 ;
 
