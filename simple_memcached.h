a3649d70a7c07f4606a6048f7d2f1a13631563fa
Linsen Ma <malinsen19980103@gmail.com>
2020-09-01 23:13:49 -0400

    Update simple_memcached.h

--- simple_memcached.h
+++ simple_memcached.h
@@ -20,13 +20,13 @@
 #define MAX_BYTES_DEFAULT 1024* 1024
 
 
-#define ITEM_key(item) (((char*)&(item->data)) \
+#define ITEM_key(item) (((char*)((item)->data)) \
          + ((item)->it_flags ? sizeof(uint64_t) : 0))
 
-#define ITEM_suffix(item) ((char*) &((item)->data) + (item)->nkey + 1 \
+#define ITEM_suffix(item) ((char*)((item)->data) + (item)->nkey + 1 \
          + ((item)->it_flags ? sizeof(uint64_t) : 0))
 
-#define ITEM_data(item) ((char*) &((item)->data) + (item)->nkey + 1 \
+#define ITEM_data(item) ((char*)((item)->data) + (item)->nkey + 1 \
          + (item)->nsuffix \
          + ((item)->it_flags ? sizeof(uint64_t) : 0))
 
